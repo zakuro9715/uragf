4f79c642428717c59cccb6e7e8403c10685017ec
YuZakuro <zakuro@yuzakuro.me>
2015-05-29 16:19:54 +0900

    Change joining api url

--- rooms/urls.py
+++ rooms/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^(?P<pk>\d+)/posts/$', views.PostList.as_view()),
     url(r'^(?P<pk>\d+)/users/$', views.UserList.as_view()),
-    url(r'^(?P<pk>\d+)/users/joining/$', views.UserJoining.as_view()),
+    url(r'^(?P<pk>\d+)/joining/$', views.UserJoining.as_view()),
 ]
