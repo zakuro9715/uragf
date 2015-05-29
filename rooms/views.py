f0cdbaf0e78414426da2145d2aca470bef0ef8d7
YuZakuro <zakuro@yuzakuro.me>
2015-05-29 15:39:39 +0900

    Add user joining api

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,8 @@
+from rest_framework import status, exceptions
+from rest_framework.response import Response
 from rest_framework.generics import (
-    ListCreateAPIView, ListAPIView, get_object_or_404
+    GenericAPIView, ListCreateAPIView, ListAPIView,
+    get_object_or_404,
 )
 
 from posts.models import Post
@@ -34,3 +37,25 @@ class UserList(ListAPIView, RoomAPIMixin):
 
     def get_queryset(self):
         return self.get_room().users.all()
+
+
+class UserJoining(GenericAPIView, RoomAPIMixin):
+    serializer_class = UserSerializer
+
+    def has_joined(self, user, room):
+        return room.users.filter(pk=user.pk).exists()
+
+    def get(self, request, *args, **kwargs):
+        if self.has_joined(request.user, self.get_room()):
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        raise exceptions.NotFound()
+
+    def put(self, request, *args, **kwargs):
+        self.get_room().users.add(request.user)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def delete(self, request, *args, **kwargs):
+        if self.has_joined(request.user, self.get_room()):
+            self.get_room().users.remove(request.user)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        raise exceptions.NotFound()
