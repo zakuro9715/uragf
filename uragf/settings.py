6ccc31c54a63642b67012fdb93ede3c6bbddaa54
YuZakuro <zakuro@yuzakuro.me>
2015-05-29 21:30:20 +0900

    Add pagination settings

--- uragf/settings.py
+++ uragf/settings.py
@@ -117,3 +117,9 @@ NOSE_ARGS = [
     '--with-coverage',
     '--cover-html',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
+}
