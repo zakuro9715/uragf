ab84e54bba1326c6ae2972253f18b68b68ec5c43
YuZakuro <zakuro@yuzakuro.me>
2015-05-29 14:25:47 +0900

    Add RoomAPIMixin

--- rooms/views.py
+++ rooms/views.py
@@ -8,9 +8,15 @@ from posts.serializers import PostSerializer
 from .models import Room
 
 
-class PostList(ListCreateAPIView):
+class RoomAPIMixin:
+    def get_room(self):
+        if not hasattr(self, 'room'):
+            self.room = get_object_or_404(Room.objects, pk=self.kwargs['pk'])
+        return self.room
+
+
+class PostList(ListCreateAPIView, RoomAPIMixin):
     serializer_class = PostSerializer
 
     def get_queryset(self):
-        room = get_object_or_404(Room.objects, pk=self.kwargs['pk'])
-        return Post.objects.filter(room=room).all()
+        return Post.objects.filter(room=self.get_room()).all()
