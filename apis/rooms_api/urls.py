bd31b4f74ddc09d2d88f32db04921909051ae1da
YuZakuro <zakuro@yuzakuro.me>
2015-06-03 14:41:58 +0900

    Move room posts api from rooms_api to room_posts_api

--- apis/rooms_api/urls.py
+++ apis/rooms_api/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.RoomList.as_view()),
-    url(r'^(?P<slug>[0-9a-zA-Z-_]+)/posts/$', views.PostList.as_view()),
     url(r'^(?P<slug>[0-9a-zA-Z-_]+)/users/$', views.UserList.as_view()),
     url(r'^(?P<slug>[0-9a-zA-Z-_]+)/joining/$', views.UserJoining.as_view()),
 ]
--- apis/rooms_api/views.py
+++ apis/rooms_api/views.py
@@ -1,11 +1,6 @@
 from rest_framework import status, exceptions, permissions
 from rest_framework.response import Response
-from rest_framework.generics import (
-    GenericAPIView, ListCreateAPIView, ListAPIView,
-)
-
-from posts.models import Post
-from posts.serializers import PostSerializer
+from rest_framework.generics import (GenericAPIView, ListAPIView)
 
 from users.serializers import UserSerializer
 
@@ -13,39 +8,10 @@ from rooms.models import Room
 from rooms.serializers import RoomSerializer
 from rooms.views import RoomMixin
 
-from .permissions import JoiningUserOnly
-
 
 class RoomList(ListAPIView):
     serializer_class = RoomSerializer
     queryset = Room.objects.all()
-
-
-class PostList(ListCreateAPIView, RoomMixin):
-    serializer_class = PostSerializer
-    permission_classes = (
-        permissions.IsAuthenticatedOrReadOnly,
-        JoiningUserOnly
-    )
-
-    def perform_create(self, serializer):
-        serializer.save(room=self.get_room(), user=self.request.user)
-
-    def get_queryset(self):
-        filters = {'room': self.get_room()}
-        from_date = self.get_from_date()
-        if from_date:
-            filters['date_created__gte'] = from_date
-        return Post.objects.filter(**filters).all()
-
-    def get_from_date(self):
-        from django.utils.dateparse import parse_datetime
-        from_str = self.request.GET.get('from', False)
-        if from_str:
-            try:
-                return parse_datetime(from_str)
-            except ValueError:
-                return None
 
 
 class UserList(ListAPIView, RoomMixin):
