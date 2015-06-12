1799f5f2f7c1cc7ac629a8c0bbe609cf266b85e2
YuZakuro <zakuro@yuzakuro.me>
2015-06-12 13:16:36 +0900

    lessコンパイル環境

---
+++ frontend/gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var lessc = require('gulp-less');
+var plumber = require('gulp-plumber');
+var path = require('path');
+var glob = require('glob');
+
+var searchApps = function(root) {
+  root = root || __dirname;
+  return glob.sync(path.join(root, '/**/__init__.py')).map(function(v) {
+    return path.dirname(v);
+  });
+};
+
+var paths = {
+  less: 'styles/*.less',
+  dist: 'static',
+};
+
+
+gulp.task('less', function() {
+  apps = searchApps();
+  options = {
+    paths: apps.map(function(v) { return path.join(v, 'styles') }),
+  }
+  apps.forEach(function(app) {
+    gulp.src(path.join(app, paths.less))
+      .pipe(lessc(options))
+      .pipe(gulp.dest(path.join(app, paths.dist)));
+  });
+});
