072cbdfa0f7a7422f7136a94b87b89e05069a6eb
YuZakuro <zakuro@yuzakuro.me>
2015-06-14 20:18:33 +0900

    homeをfrontendに移動

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,4 +4,5 @@ from .views import template as t
 urlpatterns = [
     url(r'^rooms/(?P<slug>[0-9a-zA-Z_-]+)/$', t('rooms/room.html')),
     url(r'^rooms/$', t('rooms/room_list.html')),
+    url(r'^$', t('home/home.html')),
 ]
